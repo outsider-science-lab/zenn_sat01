d51155aaae289cf23e2e0fbaca2d7df236ac023f
Kaede Fujisaki <kaede@hexe.net>
2025-08-19 06:18:24 +0900

    init

---
+++ src/zenn_sat/main.py
@@ -0,0 +1,19 @@
+from z3 import *
+
+def main():
+  a = Bool("a")
+  b = Bool("b")
+  c = Bool("c")
+  solver = Solver()
+  solver.add(And(Or([a, b]), Or([Not(a), b, Not(c)])))
+  if solver.check() == sat:
+    model = solver.model()
+    print("Satisfiable")
+    print("a = {}".format(model.eval(a)))
+    print("b = {}".format(model.eval(b)))
+    print("c = {}".format(model.eval(c)))
+  else:
+    print("Unsatisfiable")
+
+if __name__ == "__main__":
+  main()
